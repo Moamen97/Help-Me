ecb321228fdbd56a86c0e82b1e69149535a3cffe
Moamen97 <xmoamen97@gmail.com>
2018-03-18 17:03:32 +0200

    second edition

--- Project/app/src/main/java/com/helpme/MainActivity.kt
+++ Project/app/src/main/java/com/helpme/MainActivity.kt
@@ -3,16 +3,17 @@ package com.helpme
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.ViewPager
 import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val gui = GUI(this);
-        GUI.changeTypeFace(this.findViewById(R.id.Logo));
+        var SlideAdapter = slideAdapter(this);
+        var viewpager: ViewPager = findViewById(R.id.viewPager);
+        viewpager.adapter = SlideAdapter;
 
     }
 
---
+++ Project/app/src/main/java/com/helpme/slideAdapter.java
@@ -0,0 +1,74 @@
+package com.helpme;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.zip.Inflater;
+
+public class slideAdapter extends PagerAdapter {
+
+    private Context context;
+    private LayoutInflater layoutInflater;
+
+    // ourList of Images
+    private String CarpenterDescription = "Our Program includes a lot of Carpenters that they can help you fix anything you need in the field of wood or you can invite them";
+    private String PlumberDescription = "Our Program includes a lot of Plumbers that they can help you fix anything you need in the bathrooms or you can invite them";
+    private String MechanicDescription = "Our Program includes a lot of Mechanics that they can help you fix anything you need in your car or you can invite them";
+
+
+    public int[] imgs = {R.drawable.carpenter, R.drawable.plumber, R.drawable.mechanic};
+    public String[] Titles = {"Carpenter", "Plumber", "Mechanic"};
+    public String[] Description = {CarpenterDescription, PlumberDescription, MechanicDescription};
+    public int[] bkgnd = {
+            R.drawable.gradient,
+            R.drawable.gradient,
+            R.drawable.gradient,
+    };
+
+
+    public slideAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return imgs.length;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slide, container, false);
+        LinearLayout layoutSlide = (LinearLayout) view.findViewById(R.id.slideLinearLayout);
+        ImageView img = (ImageView) view.findViewById(R.id.slideImg);
+        TextView tilte = (TextView) view.findViewById(R.id.titleSlide);
+        TextView description = (TextView) view.findViewById(R.id.textDesciption);
+
+        tilte.setTextColor(Color.rgb(255, 255, 255));
+        
+        description.setTextColor(Color.rgb(255, 255, 255));
+        layoutSlide.setBackgroundResource(bkgnd[position]);
+        img.setImageResource(imgs[position]);
+        tilte.setText(Titles[position]);
+        description.setText(Description[position]);
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((LinearLayout) object);
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return (view == (LinearLayout) object);
+    }
+}
