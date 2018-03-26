237d97773d37f52a8240dd47325b00714aeb1286
Moamen97 <xmoamen97@gmail.com>
2018-03-27 00:12:31 +0200

    fix postAdapter and add click listener

--- Project/app/src/main/java/Model/postData/postAdapter.kt
+++ Project/app/src/main/java/Model/postData/postAdapter.kt
@@ -1,8 +1,10 @@
 package Model.postData
 
+import android.support.design.widget.Snackbar;
 import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import com.bumptech.glide.Glide
 import com.helpme.R
@@ -22,6 +24,16 @@ class postAdapter(var mContext: Context, var postList: ArrayList<post>, var post
         var postToBind: post = postList[position];
         holder?.postContent?.text = postToBind.postContent;
         holder?.postType?.setImageResource(postType);
+        holder?.itemView?.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                Snackbar.make(holder.view, postToBind.postContent, Snackbar.LENGTH_SHORT).show()
+            }
+        })
+        holder?.postImage?.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                Snackbar.make(holder.view, postToBind.postContent, Snackbar.LENGTH_SHORT).show()
+            }
+        })
 
         // dh by3mel load ll post cover using Glide library gamed f45
         Glide.with(mContext).load(postToBind.postImage).into(holder?.postImage);
