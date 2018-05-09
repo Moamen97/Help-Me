0424596ec7c33ec5f34c32e1b30f3af58b673ecf
MohamedAazizfcb <37511776+MohamedAazizfcb@users.noreply.github.com>
2018-05-09 17:18:17 -0300

    Done

--- Project/app/src/main/java/com/helpme/EditProfile/AddWorkShop.kt
+++ Project/app/src/main/java/com/helpme/EditProfile/AddWorkShop.kt
@@ -63,22 +63,28 @@ class AddWorkShop : AppCompatActivity(), workshopListener {
         var wname: String = (findViewById<(AutoCompleteTextView)>(R.id.WorkShopNameEditText)).text.toString();
         var wloc: String = (findViewById<(AutoCompleteTextView)>(R.id.WorkshopLocationEditText)).text.toString();
         var wnum: String = (findViewById<(AutoCompleteTextView)>(R.id.WorkshopPhoneNumberEditText)).text.toString();
-        var wid: String = (findViewById<(AutoCompleteTextView)>(R.id.WorkshopProfessionEditText)).text.toString();
+        var wid = mySelf.get_userName()
         var oname = mySelf.get_userName()
         var ofname = mySelf.get_firstName() + " " + mySelf.get_midName() + " " + mySelf.get_lastName()
         var wprof: String?
         if ((findViewById<(RadioButton)>(R.id.radioButton)).isChecked) {
             wprof = (findViewById<(RadioButton)>(R.id.radioButton)).text.toString()
+            wid +="_Clinic"
         } else if ((findViewById<(RadioButton)>(R.id.radioButton2)).isChecked) {
             wprof = (findViewById<(RadioButton)>(R.id.radioButton2)).text.toString()
+            wid +="_Engineer"
         } else if ((findViewById<(RadioButton)>(R.id.radioButton3)).isChecked) {
             wprof = (findViewById<(RadioButton)>(R.id.radioButton3)).text.toString()
+            wid +="_Plumber"
         } else if ((findViewById<(RadioButton)>(R.id.radioButton4)).isChecked) {
             wprof = (findViewById<(RadioButton)>(R.id.radioButton4)).text.toString()
+            wid +="_Mechanic"
         } else if ((findViewById<(RadioButton)>(R.id.radioButton5)).isChecked) {
             wprof = (findViewById<(RadioButton)>(R.id.radioButton5)).text.toString()
+            wid +="_Carpenter"
         } else if ((findViewById<(RadioButton)>(R.id.radioButton6)).isChecked) {
             wprof = (findViewById<(RadioButton)>(R.id.radioButton6)).text.toString()
+            wid +="_Cooking"
         } else {
             Toast.makeText(this, "UnSupported Profession", Toast.LENGTH_LONG).show();
             return
